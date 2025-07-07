0cfe2143cc1f50f3a9d96561ddceaed1b5d8f5c1
Eliyusif13 <alisadiqov20002@gmail.com>
2025-07-07 16:40:59 +0400

    first

---
+++ src/test/java/com/sadiqov/virtualfilesystem/VirtualfilesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadiqov.virtualfilesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualfilesystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
